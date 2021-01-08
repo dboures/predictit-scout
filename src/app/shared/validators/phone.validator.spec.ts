4795fc63165e9f6485c4eb808dee7f988d40753b
dboures <dboures@att.net>
2021-01-08 09:54:35 -0600

    make phone validation tests clearer

--- src/app/shared/validators/phone.validator.spec.ts
+++ src/app/shared/validators/phone.validator.spec.ts
@@ -4,17 +4,28 @@ import { phoneValidator } from '@app/shared/validators/phone.validator';
 describe('phoneValidator', () => {
     const control = new FormControl('', [Validators.required, phoneValidator]);
 
-    it('should invalidate form if no input', () => {
+    it('should reject form if no input', () => {
         expect(control.valid).toBeFalsy();
     });
 
-    it('should invalidate form if phone is bad', () => {
+    it('should reject form if phone number is poorly formed', () => {
         control.setValue('12345');
         expect(control.valid).toBeFalsy();
     });
 
-    it('should accept form if phone is good', () => {
+    it('should accept form if phone number is well formed and in US', () => {
         control.setValue('3126984237'); // cannot just be random numbers, must have legit area code
         expect(control.valid).toBeTruthy();
+    });
+
+    it('should reject form if phone number is international', () => {
+        control.setValue('49 30 901820'); 
+        expect(control.valid).toBeFalsy();
+
+        control.setValue('004930901820'); 
+        expect(control.valid).toBeFalsy();
+
+        control.setValue('+49 152 901820'); 
+        expect(control.valid).toBeFalsy();
     });
 });
